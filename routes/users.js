460bde9d32779e55ebc8b72980477eea4fc03a14
Cass Valladares <jhumphriesent@gmail.com>
2020-09-02 14:11:54 -0500

    Deploy to Heroku

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
+const { replaceOne } = require('../models/user');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index',{user: req.user} );
 });
 
 module.exports = router;
